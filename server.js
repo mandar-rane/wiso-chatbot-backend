fc914de3af7f24461d9053ae3571d66e8c48cdee
Mandar Rane <98955924+mandar-rane@users.noreply.github.com>
2024-01-07 15:40:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port =4000; // You can use any available port
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/submit_form', (req, res) => {
+  console.log(req.body)
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const slot = req.body.slot;
+
+  
+
+  // Handle the form data as needed (e.g., save to a database, perform some action)
+  console.log('Form data received:');
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Phone:', phone);
+  console.log('Slot:', slot);
+
+  const VONAGE_BRAND_NAME = 'WiseWorks';
+  const TO_NUMBER = `91${phone}`;
+  const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
+  const VONAGE_API_SECRET =  process.env.VONAGE_API_SECRET;
+
+const vonageApiUrl = 'https://rest.nexmo.com/sms/json';
+
+const requestData = {
+  from: VONAGE_BRAND_NAME,
+  text: `Appointment successfully booked. 
+  Name: ${name}
+  Slot No: ${slot}
+  `,
+  to: TO_NUMBER,
+  api_key: VONAGE_API_KEY,
+  api_secret: VONAGE_API_SECRET,
+};
+
+axios.post(vonageApiUrl, null, { params: requestData })
+  .then(response => {
+    console.log('API Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  });
+
+
+
+
+  // Respond to the chatbot user (adjust as needed)
+  res.send('Form data received successfully!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
